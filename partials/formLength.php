1208c1640ad41733224c96e55685f7580c9f6a4d
totarochristian <totaro.christian.18@gmail.com>
2023-05-05 17:02:26 +0200

    Add the submit button in the form length

--- partials/formLength.php
+++ partials/formLength.php
@@ -1,4 +1,7 @@
 <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="GET">
+  <!-- Password length -->
   <label for="passwordLength">Lunghezza:</label>
   <input type="number" name="passwordLength" id="passwordLength" min="8" max="32" value="<?php echo $passwordLength ?>">
+  <!-- Submit button -->
+  <input type="submit" value="Genera">
 </form>

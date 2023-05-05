101dbb786274df8aa6169806149c667207bac57f
totarochristian <totaro.christian.18@gmail.com>
2023-05-05 16:53:05 +0200

    Add empty general.php function file and include inside the index page

--- index.php
+++ index.php
@@ -19,5 +19,10 @@
     Milestone 3 (BONUS)
     Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente. 
   -->
+
+  <?php
+    include __DIR__ . '/functions/general.php';
+  ?>
+
 </body>
 </html>

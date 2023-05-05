7aa657a6c61af22d6351d5efd5382528a49af57f
totarochristian <totaro.christian.18@gmail.com>
2023-05-05 17:17:03 +0200

    Define the function GenerateRandomCharacter that will return a random char

--- functions/general.php
+++ functions/general.php
@@ -4,3 +4,19 @@
   function GeneratePassword(){
     return "password";
   }
+
+  function GenerateRandomCharacter(){
+    $randomNum = rand(1,3);
+    switch($randomNum){
+      case 1: 
+        $randomNum = rand(48,57);
+        break;
+      case 2: 
+        $randomNum = rand(65,90);
+        break;
+      case 3: 
+        $randomNum = rand(97,122);
+        break;
+    }
+    return chr($randomNum);
+  }

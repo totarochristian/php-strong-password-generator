23fca70de36fcc461159d6f3b72b32d7c1256ea5
totarochristian <totaro.christian.18@gmail.com>
2023-05-08 10:01:10 +0200

    Add comments to functions

--- functions/functions.php
+++ functions/functions.php
@@ -1,27 +1,37 @@
 <?php
   include __DIR__ . '/../data/data.php';
 
+  /**
+   * Function used to generate a random password with a specific length
+   * @param int $length Integer that rapresent the length of the password to be generated
+   * @return string String that rapresent the generated password
+   */
   function GeneratePassword($length){
     $pass = "";
     for($i=0; $i<$length; $i++)
-      $pass = $pass . GenerateRandomCharacter();
+      $pass .= GenerateRandomCharacter();
     return $pass;
   }
 
+  /**
+   * Function used to generate a single random character
+   * @return string String that rapresent a random single character (could be a number, a letter or a special symbol) 
+   */
   function GenerateRandomCharacter(){
     $symbols = [33,45,95,63];
+    //Random selector of general category of characters (numbers, letters (upper and down cases) and symbols)
     $randomNum = rand(1,4);
     switch($randomNum){
-      case 1: 
+      case 1: //Number, from 1 to 9
         $randomNum = rand(48,57);
         break;
-      case 2: 
+      case 2: //Letter, from a to z
         $randomNum = rand(65,90);
         break;
-      case 3: 
+      case 3: //Letter, from A to Z
         $randomNum = rand(97,122);
         break;
-      case 4:
+      case 4: //Symbol, from the symbols array
         $randomNum = $symbols[rand(0,count($symbols)-1)];
         break;
     }
